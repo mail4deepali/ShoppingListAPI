9a216a1cf7b80430f6adcc9f8477fd67ea03144b
Deepali Shah <mail4deepali@gmail.com>
2021-03-28 15:52:50 +1000

    First Draft for ShoppingListAPI

---
+++ Controllers/ShoppingListController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingListAPI.Model;
+
+namespace ShoppingListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingListController : ControllerBase
+    {
+        private readonly ShoppingListContext _context;
+
+        public ShoppingListController(ShoppingListContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/ShoppingList
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+        {
+            return await _context.Items.ToListAsync();
+        }
+
+        //GET: api/ShoppingList/1
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Item>> GetItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return item;
+        }
+
+        //POST: api/ShoppingList
+        [HttpPost]
+        public async Task<ActionResult<Item>> PostItem(Item item)
+        {
+            _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetItem", new { id = item.ItemId }, item);
+        }
+
+        //PUT: api/ShoppingList/1
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutItem(int id, Item item)
+        {
+            if(id != item.ItemId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(item).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateConcurrencyException)
+            {
+                if(!ItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        //DELETE api/ShoppingList/1
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            _context.Items.Remove(item);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+       private bool ItemExists(int id)
+        {
+            return _context.Items.Any(t => t.ItemId == id);
+        }
+    }
+}
---
+++ Model/ShoppingListContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingListAPI.Model
+{
+    public class ShoppingListContext : DbContext
+    {
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
